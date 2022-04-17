0e3937be3682e375d2feda1373c11dbf7ba6df80
Aleksey Smychenko <93550750+alfrntlower@users.noreply.github.com>
2022-04-17 21:39:23 +0300

    Task 2

--- js/02-video.js
+++ js/02-video.js
@@ -1,1 +1,28 @@
 
+const iframe = document.querySelector('iframe');
+
+const player = new Vimeo.Player(iframe);
+
+player.getVideoTitle().then(function(title) {
+        console.log('title:', title);
+});
+
+let videoplayerCurrentTime = 0;
+
+const onPlay = function(data) {
+
+    console.log('Played the video!');
+
+    videoplayerCurrentTime = data.seconds;
+
+    console.log(videoplayerCurrentTime);
+    //videoplayer-current-time
+    localStorage.setItem("videoplayer-current-time", JSON.stringify(videoplayerCurrentTime));
+
+};
+
+player.on('pause', onPlay);
+
+player.setCurrentTime(localStorage.getItem("videoplayer-current-time"));
+
+console.log(localStorage.getItem("videoplayer-current-time"));
