d774cbb94de62ec98b916f99a684163a740b310b
Aleksey Smychenko <93550750+alfrntlower@users.noreply.github.com>
2022-04-17 22:05:47 +0300

    update Task 2

--- js/02-video.js
+++ js/02-video.js
@@ -1,7 +1,11 @@
 
+import throttle from 'lodash.throttle';
+
 const iframe = document.querySelector('iframe');
 
 const player = new Vimeo.Player(iframe);
+
+const PLAYER_CURRENT_TIME = "videoplayer-current-time";
 
 player.getVideoTitle().then(function(title) {
         console.log('title:', title);
@@ -11,18 +15,18 @@ let videoplayerCurrentTime = 0;
 
 const onPlay = function(data) {
 
-    console.log('Played the video!');
-
     videoplayerCurrentTime = data.seconds;
 
     console.log(videoplayerCurrentTime);
     //videoplayer-current-time
-    localStorage.setItem("videoplayer-current-time", JSON.stringify(videoplayerCurrentTime));
+    localStorage.setItem(PLAYER_CURRENT_TIME, JSON.stringify(videoplayerCurrentTime));
 
 };
 
-player.on('pause', onPlay);
+player.on('timeupdate', throttle(onPlay, 1000));
+//player.on('pause', onPlay);
 
-player.setCurrentTime(localStorage.getItem("videoplayer-current-time"));
 
-console.log(localStorage.getItem("videoplayer-current-time"));
+player.setCurrentTime(localStorage.getItem(PLAYER_CURRENT_TIME));
+
+console.log(localStorage.getItem(PLAYER_CURRENT_TIME));
