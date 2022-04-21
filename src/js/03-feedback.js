a6f39f5c2302c66a61ef4587a171daa0d5eff44e
Aleksey Smychenko <93550750+alfrntlower@users.noreply.github.com>
2022-04-22 00:26:48 +0300

    update task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,6 +1,5 @@
 import throttle from 'lodash.throttle';
 
-
 const email = document.querySelector('[name=email]');
 const message = document.querySelector('[name=message]');
 const feedbackForm = document.querySelector('.feedback-form');
@@ -17,18 +16,23 @@ feedbackForm.addEventListener('input', throttle(onSaveFormData, 500));
 function onSubmitBtnClick(evt) {
     
     evt.preventDefault();
-  
+ 
+    if (email.value === "" || message.value === "") {
+        alert("Please fill in the empty fields");
+        return;
+    }
+
     console.log("Submit Form Done");
     console.log(formData);
-    
+
     evt.currentTarget.reset();
     localStorage.removeItem(FEEDBACK_FROM_STATE);
    
 }
 
 function onSaveFormData(evt) {
-    // console.log(evt.target.name);
-    // console.log(evt.target.value);
+    console.log(evt.target.name);
+    console.log(evt.target.value);
 
     formData[evt.target.name] = evt.target.value;
 
@@ -40,8 +44,31 @@ if (localStorage.getItem(FEEDBACK_FROM_STATE)) {
     const savedFormData = localStorage.getItem(FEEDBACK_FROM_STATE);
     const parsedFormData = JSON.parse(savedFormData);
    
-    email.value = parsedFormData.email;
-    message.value = parsedFormData.message;
+    const emailParsedFormData = parsedFormData.email;
+    const messageParsedFormData = parsedFormData.message;
+
+
+    email.value = emailParsedFormData;
+    email.textContent = emailParsedFormData;
+    message.value = messageParsedFormData;
+    message.textContent = messageParsedFormData;
+    console.log(formData);
+
+    //localStorage.setItem(FEEDBACK_FROM_STATE,JSON.stringify(formData));
+
+    // if (email.value === "undefined") {
+    //     email.value = "";
+    // } else {
+    //     email.value = parsedFormData.email;
+    // }
+
+    // if (message.value === "undefined") {
+    //     message.value = "";
+    // } else {
+    //     message.value = parsedFormData.message;
+    // }
+    
+
        
 }
 
